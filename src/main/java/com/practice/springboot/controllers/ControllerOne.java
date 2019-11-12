19b7d05158310ea17d7b2585a03fa97b62689c1d
Arghya Pal <palarghya337@gmail.com>
2019-11-12 10:00:16 +0530

    Fixed issue
    Moved Main class to the root package above other classes, the reason of moving given below -
    
    When you run a Spring Boot Application, (i.e. a class annotated with @SpringBootApplication), Spring will only scan the classes below your main class package.

--- src/main/java/com/practice/springboot/controllers/ControllerOne.java
+++ src/main/java/com/practice/springboot/controllers/ControllerOne.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ControllerOne {
 
-	@RequestMapping(value = "/")
+	@RequestMapping("/welcome")
 	public String requestMapping() {
 		return "Here you go... First Spring Boot Run";
 	}
